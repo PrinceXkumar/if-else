001600075257968f2ccbfab54c6d0333fdfc0c03
PrinceXkumar <118840584+PrinceXkumar@users.noreply.github.com>
2023-11-30 00:17:21 +0530

    Add files via upload
    if else questions.

---
+++ if.cpp
@@ -0,0 +1,42 @@
+/*
+// Find greatest between three numbers
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin>>a>>b>>c;
+    if(a>b && a>c)
+    {
+        cout<<"A is greater than B and C.";
+    }
+    else if(b > c  && b > a)
+    {
+        cout<<"B is greater than  A and C.";
+    }
+    else 
+    {
+        cout<<"C is greater than A and B.";
+    }
+}
+*/
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int length, breath;
+//     int area;
+//     cout<<"Enter the value : ";
+//     cin>>length>>breath;
+//     area= length*breath;
+//     if(area==length*breath)
+//     {
+//         cout<<"It is square.";
+//     }
+//     else{
+//         cout<<"It is rectangle.";
+//     }
+
+// }
